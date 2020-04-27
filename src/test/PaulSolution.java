515df340dd5dc96f5ed16b1082e1ff430b6ab576
jaspreetkaur <jaspreetkaur@10.0.0.102>
2020-04-27 15:26:56 -0400

    Initial commit a project

---
+++ src/test/PaulSolution.java
@@ -0,0 +1,32 @@
+package test;
+
+
+
+import java.util.*;
+
+class YN
+{
+public static void main(String args[])
+{
+char sAns;
+boolean bContinue=true;
+Scanner cin=new Scanner(System.in);
+do
+{
+System.out.print("\nContinue? Y or N: ");
+sAns=cin.next().charAt(0);
+if(sAns=='Y' || sAns=='N')
+{
+System.out.println("user types "+sAns+", loop exists\n");
+bContinue=false;
+}
+else
+{
+System.out.println("user types "+sAns+"\nError Y or N only\n");
+
+}
+}
+while(bContinue==true);
+}
+}
+
